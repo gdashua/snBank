11f4b0d75965c671fd15ae72241709341af16c69
gdashua <gdashua@gmail.com>
2020-05-12 15:10:07 +0100

    Update pythonTask4.py

--- pythonTask4.py
+++ pythonTask4.py
@@ -1,48 +1,89 @@
+import re
+import os
+import random
 
- #creates staff.txt file
- staff = open("staff.txt", "w");
- #create staff information to enable login
- staff.write("staffs = { 'staff1': {'userName': 'Jpee', 'password':'jstart.ng', 'email':'j@gmail.com'},'staff2': {'userName': 'kobey', 'password':'kstart.ng', 'email':'k@gmail.com'}}");
- staff.close();
 
- #creates customer.txt file
-customer = open("customer.txt", "w");
-customer.close();
+def createBankAcc(a,b,c,d):
+  customer = open('customer.txt', 'a')
+  customer.write(a)
+  customer.write(b)
+  customer.write(c)
+  customer.write(d)
+  passw=""
+  for i in range(1,10):
+    passw+= str(random.randrange(1,9))
 
-	#staff login
- def login():
- 	print 'enter user name and password'
- 	x = input("user name: ")
- 	x = input("password: ")
- 	#code written into staff.txt to loop through the staff details for authentication
- 	staff.write("/n foreach i staff{}:/n if staff[i][userName]==x && staff[i][password]==y /n open('session.txt','x') /n print('1. Create account')/n print('2. Check account detail')/n print('3. logout') ")
- 	exec(staff.oper(staff.txt).read())#runs the staff.txt file to run the python script
+    customer.write(passw)
+  print('The customer\'s password is: '+passw )
+  print('You have been logged in')
+  print('1. Create new bank account')
+  print('2. Check account details')
+  print('3. logout')
+  x = input('Enter an option')
+  if x=='1':
+      print('Enter the following details please: ')
+      a = input('Account Name: ')
+      b = input('Opening Balance: ')
+      c = input('Account Type: ')
+      d = input('Account Email: ')
+      createBankAcc(a,b,c,d)
+  elif x=='2':
+      checkAppDet()
+  else:
+      logout()
 
- 	x = input('enter your option here: ')
- 	#conditional statement to determine next line of action after staff has made a choose of action
- 	if x=='1':
- 		w= input("enter preferred account name: ")
- 		x= input("enter openning balance: ")
- 		y= input("enter account type: ")
- 		z= input("enter account email: ")
+  
+def login(userName, pword):
+  myStr= open('staff.txt', 'r').read()
+  y= str(myStr)
+  x = re.search(userName, y )
+  userName=x.group()
+  z= re.search(pword,y)
+  pword= z.group()
+  if pword=='none' or userName=='none':
+    print('wrong password or username')
+    start()
+  else:
+    session= open('session.txt','w')
+    session.write(userName)
+    print('You have been logged in')
+    print('1. Create new bank account')
+    print('2. Check account details')
+    print('3. logout')
+    x = input('Enter an option')
+    if x=='1':
+      print('Enter the following details please: ')
+      a = str(input('Account Name: '))
+      b = str(input('Opening Balance: '))
+      c = str(input('Account Type: '))
+      d = str(input('Account Email: '))
+      createBankAcc(a,b,c,d)
+    #elif x=='2':
+      #checkAppDet()
+    #else:
+      #logout()
 
-#compile new customer informatio to be sent the customer.txt
- customer.write(customers = {'custormer1'= {'account name': w, 'openning balance': x, 'account type': y, 'account email':z}})
-#loop to generate 10 digit Account Number for new customer
-for i in range(1,6):
-  pass1+= str(random.randrange(1,9))
+def start():
+  print('What would you like to do?')
+  print('1. Login')
+  print('2. Close App\nenter 1 or 2')
+  x = input()
 
-  print('the account password is: '+ pass1)
+  if x=='1':
+    print('Enter your user name')
+    u = input()
+    print('Enter your password')
+    v = input()
+    login(u,v)
+    if x=='1':
+      print('Enter you password')
+    x = input()
 
-#function to start the program
- def start():
-	 print('Choose one of the options below');
-	 print('1. Login');
-	 print('2. Close App');
-	 x = input('enter 1 or 2:');
+  else:
+    exit()
+    
+start()
 
-	 if x==1:
-	 	login();
-	 elif x==2:
-	 	closeApp();
-	 else start();
+
+
+
